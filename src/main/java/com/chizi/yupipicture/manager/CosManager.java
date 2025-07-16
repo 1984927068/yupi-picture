20a4183668cbc48300fa257df35a2c539f106a8c
1984927068 <1984927068@qq.com>
2025-07-16 22:41:20 +0800

    图片管理

---
+++ src/main/java/com/chizi/yupipicture/manager/CosManager.java
@@ -0,0 +1,29 @@
+package com.chizi.yupipicture.manager;
+
+import com.chizi.yupipicture.config.CosClientConfig;
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.exception.CosClientException;
+import com.qcloud.cos.exception.CosServiceException;
+import com.qcloud.cos.model.PutObjectRequest;
+import com.qcloud.cos.model.PutObjectResult;
+import com.qcloud.cos.transfer.Upload;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.io.File;
+
+@Component
+public class CosManager {
+
+    @Resource
+    private CosClientConfig cosClientConfig;
+
+    @Resource
+    private COSClient cosClient;
+
+    // 上传对象
+    public PutObjectResult putObject(String key, File file) {
+        PutObjectRequest putObjectRequest = new PutObjectRequest(cosClientConfig.getBucket(), key, file);
+        return cosClient.putObject(putObjectRequest);
+    }
+}
